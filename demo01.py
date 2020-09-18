a1cfabcff0d1650827648098d9e67c08413c89a5
pigeon0715 <1114944205@qq.com>
2020-09-18 21:38:16 +0800

    第一次向Github提交代码

---
+++ demo01.py
@@ -0,0 +1,37 @@
+'''
+print("hello,world!") # 字符串
+print(2333) # 整数
+print(2.333) # 小数
+print(True) # 布尔值 true false
+print(()) # 元组
+print([]) # 数组
+print({}) # 字典
+print("哈哈"*10)
+print("哈哈"+"嘻嘻") #字符串的拼接
+print("哈哈","嘻嘻")
+print(1+1)
+print(2>3)
+
+a=input("请输入：")
+print("input获取的内容：",a)
+
+print(type("hello,world!"))
+print(type(2333)) 
+print(type(2.333)) 
+print(type(True)) 
+print(type(()))
+print(type([])) 
+print(type({}))
+
+
+a=str(2333)
+print(type(a))
+
+a = float(input("请输入："))
+b = float(input("请输入："))
+print("a+b=",a+b)
+
+'''
+a = input("请输入：")
+b = input("请输入：")
+print("两段字符的长度=",len(a)+len(b))
